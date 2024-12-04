bfc67983deeba04c266c10433bfd932fe0710736
Sengeki1 <gamersengeki@gmail.com>
2024-12-04 14:07:23 -0100

    Added a Toolbar (enables the menu to be rendered at the main activity)

--- app/src/main/java/com/example/menu_app/MainActivity.java
+++ app/src/main/java/com/example/menu_app/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.menu_app;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -21,11 +27,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);  // This sets your toolbar as the action bar
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("MenuTest", "onCreateOptionsMenu called");
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.option_favorites) {
+            Toast.makeText(this, "Test Item Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.option_settings) {
+            Toast.makeText(this, "Test Item Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
